37ed4cc748c6de6f4d9453d2422c9a96800df33d
artyom123 <artvirsh@mail.ru>
2018-02-27 00:24:52 +0300

    longest consecutive sequence decided

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+ 
+    let k = 1, arr = [];
+    if(array.length === 0) return 0;
+    else if(array.length === 1) return 1;
+
+    array = Array.from(new Set(array.sort((a,b)=>a-b)));
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] + 1 === array[i+1]) k++;
+        else{
+            arr.push(k);
+            k = 1;
+        }
+    }
+
+    return arr.sort((a,b)=>b-a)[0];
+
 }
